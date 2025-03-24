f85621d64c488c806889a4e621192a21255b8034
Everest Paudel <paudeleverest09@gmail.com>
2025-03-24 11:03:25 +0545

    Refactor RoomRepo to return Room instead of Optional; add MongoDB database environment variable in Docker Compose

--- src/main/java/com/offnine/chat_app_backend/Repo/RoomRepo.java
+++ src/main/java/com/offnine/chat_app_backend/Repo/RoomRepo.java
@@ -1,14 +1,17 @@
 package com.offnine.chat_app_backend.Repo;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.stereotype.Repository;
-import com.offnine.chat_app_backend.entities.Room;
 
+import com.offnine.chat_app_backend.entities.Room;
+import java.util.List;
 import java.util.Optional;
 
-@Repository
-public interface RoomRepo extends MongoRepository<Room, String> {
 
-    // Get room using room ID (returns Optional to avoid NullPointerException)
-    Optional<Room> findByRoomId(String roomId);
+@Repository
+public interface RoomRepo extends MongoRepository<Room,String>{
+
+    // get  room using room id
+  Room  findByRoomId(String roomId);
 }
